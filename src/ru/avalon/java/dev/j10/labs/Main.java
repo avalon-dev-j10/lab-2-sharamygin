54d2e94e99de1123d9fdc8872719589bb507ccd7
sera <sera@172.20.10.2>
2019-03-25 01:31:05 +0300

    25 march 2019 v1.0 Done.
    // Для чего необходим rotation?

--- src/ru/avalon/java/dev/j10/labs/Main.java
+++ src/ru/avalon/java/dev/j10/labs/Main.java
@@ -1,11 +1,37 @@
 package ru.avalon.java.dev.j10.labs;
 
-import ru.avalon.java.dev.j10.labs.shapes.Shape;
+import ru.avalon.java.dev.j10.labs.shapes.*;
 
 public class Main {
 
     public static void main(String[] args) {
-        Shape[] shapes = null;
+        Shape[] shapes = new Shape[10];
+        shapes[0] = (Shape) new Circle();
+        shapes[1] = (Shape) new Circle();
+        shapes[2] = (Shape) new Circle();
+        shapes[3] = (Shape) new Rectangle();
+        shapes[4] = (Shape) new Rectangle();
+        shapes[5] = (Shape) new Rectangle();
+        shapes[6] = (Shape) new Rectangle();
+        shapes[7] = (Shape) new Triangle();
+        shapes[8] = (Shape) new Triangle();
+        shapes[9] = (Shape) new Triangle();
+
+        Shape max = null;
+        
+        for(int i = 0; i < shapes.length; i++){
+            System.out.println(shapes[i] + "|Area " + shapes[i].getArea() + " m2|"); 
+            if(max != null){          
+                if(shapes[i].getArea() > max.getArea()){
+                    max = shapes[i];
+                }
+            }
+                else {
+                    max = shapes[i];
+                }
+        }
+            
+        System.out.println("\n" + "Max area for figure " + max + "is " + max.getArea() + " m2"); 
 
         /*
          * TODO: Выполнить действия над массивом 'shapes'
@@ -18,6 +44,6 @@ public class Main {
          * 2. Найти в массиве 'shapes' фигуру с максимальной
          *    площадью. Для поиска фигуры необходимо создать
          *    статический метод в текущем классе (Main).
-         */
+         */ 
     }
 }
--- src/ru/avalon/java/dev/j10/labs/shapes/Rectangle.java
+++ src/ru/avalon/java/dev/j10/labs/shapes/Rectangle.java
@@ -1,5 +1,7 @@
 package ru.avalon.java.dev.j10.labs.shapes;
 
+import static java.lang.Math.PI;
+
 /**
  * Представление о прямоугольнике.
  * <p>
@@ -8,11 +10,59 @@ package ru.avalon.java.dev.j10.labs.shapes;
  *
  * @see <a href="https://ru.wikipedia.org/wiki/%D0%9F%D1%80%D1%8F%D0%BC%D0%BE%D1%83%D0%B3%D0%BE%D0%BB%D1%8C%D0%BD%D0%B8%D0%BA">Прямоугольник</a>
  */
-public class Rectangle {
+public class Rectangle implements Point, Polygon, Shape{
 
     /*
      * TODO: Реализовать класс 'Rectangle'
      * 1. Используйте наследование.
      * 2. Реализуйте все абстрактные методы.
      */
+    
+    private final float a;
+    private final float b;
+    private final float x;
+    private final float y;
+    private final int rotation;
+    
+    
+    public Rectangle(){
+        a = (float) Math.random();
+        b = (float) Math.random();
+        x = (float) Math.random();
+        y = (float) Math.random();
+        rotation = (int) (360*Math.random());
+    }
+    
+    @Override
+    public float getX(){
+        return x;
+    }
+    
+    @Override
+    public float getY(){
+        return y;
+    }
+    
+    
+    @Override
+    public float getPerimeter(){
+        return (float) (2*a+2*b);
+    }
+    
+    
+    @Override
+    public float getArea(){
+        return (float) (a*b);
+    }
+    
+    @Override
+    public int getRotation(){
+        return rotation;
+    }
+        
+        @Override
+    public String toString(){
+        return "|Rectangle (perimeter " + getPerimeter() + " m)| ";
+}
+    
 }
